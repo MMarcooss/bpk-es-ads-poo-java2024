3b707212e8d93254a74504e7c97f999c3e798623
MMarcooss <marquinhostavares03@gmail.com>
2024-10-29 19:18:33 -0300

    Atualização

---
+++ Aula 8/src/Veiculos/Carro.java
@@ -0,0 +1,17 @@
+public class Carro extends Veiculo {
+
+    private Integer numeroPortas;
+
+    public Carro(String marca, int ano, String modelo) {
+        super(marca, ano, modelo);
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                ", ano=" + ano +
+                ", modelo='" + modelo + '\'' +
+                ", marca='" + marca + '\'' +
+                '}';
+    }
+}
---
+++ Aula 8/src/Veiculos/Veiculo.java
@@ -0,0 +1,12 @@
+public class Veiculo {
+
+    protected String marca;
+    protected String modelo;
+    protected Integer ano;
+
+    public Veiculo(String marca, int ano, String modelo) {
+        this.marca = marca;
+        this.ano = ano;
+        this.modelo = modelo;
+    }
+}
